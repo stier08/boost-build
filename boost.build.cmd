3cd760b1d54dfd008c6b16cca8ec5adb9417f040
stier08 <stier08@gmail.com>
2017-10-21 16:56:11 +0200

    Update (a) WinPython-32bit-3.4.4.7Zero (b) VS2017

--- boost.build.cmd
+++ boost.build.cmd
@@ -1,38 +1,54 @@
-SET PYTHONHOME=D:\Soft\Python\35\WinPython-32bit-3.5.3.0Zero\python-3.5.3
-set PATH=%PYTHONHOME%;%PYTHONHOME%\Scripts;%PATH%
+rem set PATH=P:\Portable_Python_3.2.5.1\App;P:\Portable_Python_3.2.5.1\App\Scripts;%PATH%
+set PATH=P:\WinPython-32bit-3.4.4.7Zero\python-3.4.4;P:\WinPython-32bit-3.4.4.7Zero\python-3.4.4\Scripts;%PATH%
+rem set PATH=D:\Python34;D:\Python34\Scripts;%PATH%
+
+set VS150COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\
 
 
-set BOOST_ROOT=D:\LIBS\boost\boost-1_63_0_vs2015_wp35
+set BOOST_ROOT=D:\LIBS\boost\boost-1_65_1_vs2017_wp34
 set BOOST_BUILD_ROOT=%BOOST_ROOT%
 set BOOST_TARGET_ROOT=%BOOST_ROOT%
-call "%VS140COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
+call "%VS150COMNTOOLS%..\..\VC\Auxiliary\Build\vcvars32.bat"
 cd /d %BOOST_ROOT%
 
-set vsMainVersion=14.0
+if exist b2.exe (
+echo "b2.exe exists"
+) else (
+ call bootstrap.bat --with-libraries=python
+)
+rem call bootstrap.bat
+
+set vsMainVersion=14.1
 set BuildDir=%BOOST_BUILD_ROOT%\build
 set PrefixDir=%BOOST_TARGET_ROOT%
 set IncludeDir=%BOOST_TARGET_ROOT%\include
 set LibDir=%BOOST_TARGET_ROOT%\lib
-echo "========================================================================"
-rem call bootstrap.bat --with-libraries=python
-rem b2 ^
-rem  --build-dir=%BuildDir% ^
-rem  --prefix=%PrefixDir% ^
-rem  --includedir=%IncludeDir% ^
-rem  --libdir=%LibDir% ^
-rem  --with-python ^
-rem  --build-type=complete ^
-rem  link=shared ^
-rem  toolset=msvc-%vsMainVersion% ^
-rem  stage install
 
-call bootstrap.bat
+echo "===============Building With Python Libraries========================================================="
 b2 ^
   --build-dir=%BuildDir% ^
   --prefix=%PrefixDir% ^
   --includedir=%IncludeDir% ^
   --libdir=%LibDir% ^
+  --with-python ^
   --build-type=complete ^
   link=shared ^
   toolset=msvc-%vsMainVersion% ^
   stage install
+
+
+echo "===============Building Without Python Libraries========================================================="
+
+b2 ^
+  --build-dir=%BuildDir% ^
+  --prefix=%PrefixDir% ^
+  --includedir=%IncludeDir% ^
+  --libdir=%LibDir% ^
+  --without-python ^
+  --without-mpi ^
+  link=shared ^
+  toolset=msvc-%vsMainVersion% ^
+  stage install
+
+
+
