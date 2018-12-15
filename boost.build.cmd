f783aa426f40ee9ac715eb5940b2a877a978a398
stier08 <stier08@gmail.com>
2018-12-15 19:29:28 +0100

    SET BOOST_ARCHITECTURE=x86 SET BOOST_ADDRESS_MODEL=32

--- boost.build.cmd
+++ boost.build.cmd
@@ -3,15 +3,16 @@ rem set PYTHONHOME=P:\WinPython-32bit-3.4.4.7Zero\python-3.4.4
 set PATH=%PYTHONHOME%;%PYTHONHOME%\Scripts;%PATH%
 rem set PATH=D:\Python34;D:\Python34\Scripts;%PATH%
 
-set VS140COMNTOOLS=D:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\
+set VS140COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\
 
 rem http://www.boost.org/build/doc/html/bbv2/overview/invocation.html
-set BOOST_ROOT=D:\LIBS\boost\boost_1_68_0_vs2015_wp37
+set BOOST_ROOT=D:\LIBS\boost\boost-1_68_0_vs2015_wp37
 set BOOST_BUILD_ROOT=%BOOST_ROOT%
 set BOOST_TARGET_ROOT=%BOOST_ROOT%
 set BOOST_BUILD_CONFIG=
 set BOOST_BUILD_CONFIG=--debug-configuration  --debug-building --debug-generators -d 2
-
+SET BOOST_ARCHITECTURE=x86
+SET BOOST_ADDRESS_MODEL=32
 python -c "from sys import *; print('version=%d.%d\nplatform=%s\nprefix=%s\nexec_prefix=%s\nexecutable=%s' % (version_info[0],version_info[1],platform,prefix,exec_prefix,executable))" 2>&1
 
 
@@ -33,7 +34,7 @@ set LibDir=%BOOST_TARGET_ROOT%\lib
 set BOOST_THEADING=multi
 set BOOST_RUNTIME_LINK=shared
 rem (static|shared)
-set BOOST_LINK=shared
+set BOOST_LINK=static
 
 
 if "%BOOST_LINK%"=="static" (
@@ -54,6 +55,8 @@ b2 ^
   --with-python ^
   --build-type=complete ^
   %BOOST_BUILD_CONFIG% ^
+  architecture=%BOOST_ARCHITECTURE% ^
+  address-model=%BOOST_ADDRESS_MODEL% ^ 
   threading=%BOOST_THEADING% ^
   runtime-link=%BOOST_RUNTIME_LINK% ^
   link=%BOOST_LINK% ^
@@ -62,7 +65,6 @@ b2 ^
   stage install
 
 echo "===============Python Libraries Done========================================================="
-
 
 echo "===============Building With test Libraries========================================================="
 b2 ^
@@ -73,6 +75,8 @@ b2 ^
   --with-test ^
   --build-type=complete ^
   %BOOST_BUILD_CONFIG% ^
+  architecture=%BOOST_ARCHITECTURE% ^
+  address-model=%BOOST_ADDRESS_MODEL% ^ 
   threading=%BOOST_THEADING% ^
   runtime-link=%BOOST_RUNTIME_LINK% ^
   link=%BOOST_LINK% ^
@@ -91,15 +95,14 @@ b2 ^
   --with-regex ^
   --build-type=complete ^
   %BOOST_BUILD_CONFIG% ^
+  architecture=%BOOST_ARCHITECTURE% ^
+  address-model=%BOOST_ADDRESS_MODEL% ^ 
   threading=%BOOST_THEADING% ^
   runtime-link=%BOOST_RUNTIME_LINK% ^
   link=%BOOST_LINK% ^
   toolset=msvc-%vsMainVersion% ^
   %B2_DEFINES% ^
   stage install
-
-
-
 
 echo "===============Building Without Python Libraries========================================================="
 
@@ -111,6 +114,8 @@ b2 ^
   --without-python ^
   --without-mpi ^
   %BOOST_BUILD_CONFIG% ^
+  architecture=%BOOST_ARCHITECTURE% ^
+  address-model=%BOOST_ADDRESS_MODEL% ^ 
   threading=%BOOST_THEADING% ^
   runtime-link=%BOOST_RUNTIME_LINK% ^
   link=%BOOST_LINK% ^
